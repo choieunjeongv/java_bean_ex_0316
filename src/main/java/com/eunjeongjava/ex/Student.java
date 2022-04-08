3d76777f0352337117359b5602e34f1185a4f8a5
choieunheong <mjchoi17@hanmail.net>
2022-04-08 14:34:24 +0900

    자바빈 연습

---
+++ src/main/java/com/eunjeongjava/ex/Student.java
@@ -0,0 +1,47 @@
+package com.eunjeongjava.ex;
+
+public class Student {
+
+	private String name;//학생나이
+	private int age;//나이
+	private int grade;//학년
+	private int studentNum;//학번
+	
+	public Student() {
+		super();	
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+
+	public int getStudentNum() {
+		return studentNum;
+	}
+
+	public void setStudentNum(int studentNum) {
+		this.studentNum = studentNum;
+	}
+
+	
+}
